4d77f008cfa8f5cfbc356cf4535e4526eea87930
Lekuru <contact@lekuru.xyz>
2024-01-06 14:50:58 +0100

    Update config

--- config.py
+++ config.py
@@ -16,9 +16,6 @@ S3_ACCESS_KEY = os.environ.get('S3_ACCESS_KEY')
 S3_SECRET_KEY = os.environ.get('S3_SECRET_KEY')
 S3_BASEURL    = os.environ.get('S3_BASEURL')
 
-SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
-SENDGRID_EMAIL = os.environ.get('SENDGRID_EMAIL')
-
 REDIS_HOST = os.environ.get('REDIS_HOST')
 REDIS_PORT = int(os.environ.get('REDIS_PORT', 6379))
 
@@ -30,3 +27,13 @@ S3_ENABLED = eval(os.environ.get('ENABLE_S3', 'True').capitalize())
 DEBUG = eval(os.environ.get('DEBUG', 'False').capitalize())
 
 DATA_PATH = os.path.abspath('.data')
+
+SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
+SENDGRID_EMAIL = os.environ.get('SENDGRID_EMAIL')
+
+MAILGUN_API_KEY = os.environ.get('MAILGUN_API_KEY')
+MAILGUN_EMAIL = os.environ.get('MAILGUN_EMAIL', '')
+MAILGUN_URL = os.environ.get('MAILGUN_URL', 'api.eu.mailgun.net')
+MAILGUN_DOMAIN = MAILGUN_EMAIL.split('@')[-1]
+
+OFFICER_WEBHOOK_URL = os.environ.get('OFFICER_WEBHOOK_URL')
