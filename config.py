dbdb7eb475815faec52c2ccbc7e6a0accde2cf33
Lekuru <contact@lekuru.xyz>
2025-01-11 18:09:06 +0100

    Update config

--- config.py
+++ config.py
@@ -8,7 +8,6 @@ POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
 POSTGRES_PORT = int(os.environ.get('POSTGRES_PORT', 5432))
 POSTGRES_USER = os.environ.get('POSTGRES_USER')
 POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
-
 POSTGRES_POOLSIZE = int(os.environ.get('POSTGRES_POOLSIZE', 10))
 POSTGRES_POOLSIZE_OVERFLOW = int(os.environ.get('POSTGRES_POOLSIZE_OVERFLOW', 30))
 
@@ -19,13 +18,12 @@ S3_BASEURL    = os.environ.get('S3_BASEURL')
 REDIS_HOST = os.environ.get('REDIS_HOST')
 REDIS_PORT = int(os.environ.get('REDIS_PORT', 6379))
 
-DOMAIN_NAME = os.environ.get('DOMAIN_NAME')
-
 APPROVED_MAP_REWARDS = eval(os.environ.get('APPROVED_MAP_REWARDS', 'False').capitalize())
 FREE_SUPPORTER = eval(os.environ.get('FREE_SUPPORTER', 'True').capitalize())
 ENABLE_SSL = eval(os.environ.get('ENABLE_SSL', 'False').capitalize())
 S3_ENABLED = eval(os.environ.get('ENABLE_S3', 'True').capitalize())
 DEBUG = eval(os.environ.get('DEBUG', 'False').capitalize())
+DOMAIN_NAME = os.environ.get('DOMAIN_NAME')
 
 EMAIL_PROVIDER = os.environ.get('EMAIL_PROVIDER')
 EMAIL_SENDER = os.environ.get('EMAIL_SENDER')
